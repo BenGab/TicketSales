4241c25ac28eddcff2119d30438fcede33723f3b
Gabor Benko <benkogabor@msn.com>
2020-10-13 20:58:32 +0200

    model creation

---
+++ TicketShop.DB/Models/Seller.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TicketShop.DB.Models
+{
+    [Table("sellers")]
+    public class Seller
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [ForeignKey(nameof(Venue))]
+        public int VenueId { get; set; }
+
+        [NotMapped]
+        public virtual Venue Venue { get; set; }
+    }
+}
